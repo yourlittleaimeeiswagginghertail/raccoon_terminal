04b22cbdd206503d8cf395583169475f7470c4e5
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2022-11-11 19:31:25 +0300

    input order-number

--- task_1.py
+++ task_1.py
@@ -1,12 +1,12 @@
-def trying_1():
+def trying_1(masu_1):
     try:
-        return float(input("введи число: "))
+        return float(input("введи " + str(masu_1 + 1) + "-ое " + "число: "))
     except ValueError:
         return "значением функции является сейчас этот текст"
 
 numbers = []
 for masu_1 in range(100):    
-    catched_number = trying_1()
+    catched_number = trying_1(masu_1)
     if type(catched_number) != float:
         print(catched_number)
     else:
