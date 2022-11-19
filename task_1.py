72a533b019103bc6cf45c614dc6348b2c54be24a
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2022-11-19 15:27:49 +0300

    task_2

--- task_1.py
+++ task_1.py
@@ -1,12 +1,17 @@
 import numpy as np
+from numpy import random
 
-#создать array
-bath = np.arange(51)
 
-#c десятого индекса, до последнего, шаг два
-birthday = bath[10: :2]
-print(birthday)
 
-#в обратном порядке
-sympathy = np.flip(birthday, 0)
-print(sympathy)
+#куб 3 на 3 на 3
+bike = random.randint(10, 20, size=(3, 3, 3))
+print(bike)
+
+#наименьш из array
+roof = np.amin(bike)
+print(roof)
+
+#макс из array
+building = np.amax(bike)
+print(building)
+
