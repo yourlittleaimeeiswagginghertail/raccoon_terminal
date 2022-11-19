ceace4c0b8a758b57698e374b988654bb3d2571e
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2022-11-19 21:25:23 +0300

    task_513

--- task_1.py
+++ task_1.py
@@ -1,17 +1,32 @@
 import numpy as np
 from numpy import random
+"""
+#прямоугольник из нолей
+truck = np.zeros( (3, 4), dtype = int)
+print(truck)
 
 
+#прямоугольник из единиц
+truck2 = np.ones( (5, 6), dtype = int)
+print(truck2)
 
-#куб 3 на 3 на 3
-bike = random.randint(10, 20, size=(3, 3, 3))
-print(bike)
+lin = 5
+col = 6
 
-#наименьш из array
-roof = np.amin(bike)
-print(roof)
+bike = np.random.randint(1, 10, size=(lin, col))
+print(bike, "\n")
 
-#макс из array
-building = np.amax(bike)
-print(building)
+road = bike[1:(lin-1), 1:(col-1)]
+print(road)
+"""
 
+text = input("enter 2 numbers: ")
+cat = text.split(" ")
+lin, col = map(int, cat)
+
+#прямоугольник из единиц
+truck2 = np.ones( (lin, col), dtype = int)
+#print(truck2)
+
+truck2[1:(lin-1), 1:(col-1)] = 0
+print(truck2)
--- task_2.py
+++ task_2.py
@@ -1,2 +1,11 @@
-print("hello")
-print(round(4.6))
+import numpy as np
+
+#от [10 до 51) и шаг 2
+glass = np.arange(10, 51, 2)
+print(glass)
+
+#в обратном порядке
+light = np.flip(glass, 0)
+print(light)
+
+
