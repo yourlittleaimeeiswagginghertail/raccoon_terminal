c13429767fea781c49f0b43ea6ffd007fd4c6df8
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2022-11-23 18:12:13 +0300

    515-серединка из набора нормального

--- task_1.py
+++ task_1.py
@@ -1,32 +1,91 @@
+"""
+import pylab
+
+x = pylab.linspace(-6, 6, 1000)
+
+def gaussian(x, sigma):
+    #Return the normalized Gaussian with standard deviation sigma
+    c = pylab.sqrt(2 * pylab.pi)
+    return pylab.exp(-0.5 * (x / sigma)**2) / sigma / c
+
+sigma1 = 1
+g1 = gaussian(x, sigma1)
+
+pylab.plot(x, g1)
+
+pylab.show()
+
+"""
+
+"""
+import pylab
+x = 3
+sigma = 5
+
+def gaus(x, sigma):
+    return round(1/(sigma*pylab.sqrt(2*pylab.pi))*pylab.exp(-1*x**2/(2*sigma**2)), 5)
+
+
+print(gaus(x, sigma))
+
+"""
+"""
 import numpy as np
-from numpy import random
+
+bone = np.random.normal(loc=10,  scale=5, size=(20, 1))
+print(bone, "\n")
+
+skeleton = np.sort(bone, axis=0)
+print(skeleton)
+
+#skeleton = np.sort(coffin, axis=0)
+#print(skeleton, "\n")
+
 """
-#прямоугольник из нолей
-truck = np.zeros( (3, 4), dtype = int)
-print(truck)
+import numpy as np
 
+for repeat in range(1):
+	mu1 = float(5)
+	sigma1 = float(2)
+	amo_num = int(100)
 
-#прямоугольник из единиц
-truck2 = np.ones( (5, 6), dtype = int)
-print(truck2)
+	bone1 = np.random.normal(loc=mu1,scale=sigma1,size=(amo_num))
+	bone2 = np.around(bone1, 3)
+	bone3 = np.sort(bone2, axis=0)
+	print("numbers: ", bone3, "\n")
+	
+"""
+	mu2 = np.average(bone3)
+	print("*****"*6, round(mu2, 3))
 
-lin = 5
-col = 6
-
-bike = np.random.randint(1, 10, size=(lin, col))
-print(bike, "\n")
-
-road = bike[1:(lin-1), 1:(col-1)]
-print(road)
+	sigma2 = np.std(bone3)
+	print("*****"*6, round(sigma2, 3), "\n")
 """
 
-text = input("enter 2 numbers: ")
-cat = text.split(" ")
-lin, col = map(int, cat)
 
-#прямоугольник из единиц
-truck2 = np.ones( (lin, col), dtype = int)
-#print(truck2)
+coffin = bone3[ ( bone3>=(mu1-sigma1) ) & ( bone3<=(mu1+sigma1) ) ]
+print("отличаются от среднего не более чем на одну сигму:", coffin)
 
-truck2[1:(lin-1), 1:(col-1)] = 0
-print(truck2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
