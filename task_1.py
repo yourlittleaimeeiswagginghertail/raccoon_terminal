527c19f13b93507e4a3bd2121dd14db54a912633
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2022-11-23 18:26:53 +0300

    graph from textbook

--- task_1.py
+++ task_1.py
@@ -1,18 +1,1 @@
-"""
-import pylab
-
-x = pylab.linspace(-6, 6, 1000)
-
-def gaussian(x, sigma):
-    #Return the normalized Gaussian with standard deviation sigma
-    c = pylab.sqrt(2 * pylab.pi)
-    return pylab.exp(-0.5 * (x / sigma)**2) / sigma / c
-
-sigma1 = 1
-g1 = gaussian(x, sigma1)
-
-pylab.plot(x, g1)
-
-pylab.show()
-
-"""
+print("hello")
