29d08e64721e75d2bb5a762252bea717c639c6ca
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2022-11-25 20:45:26 +0300

    update

--- task_1.py
+++ task_1.py
@@ -32,32 +32,7 @@ print("способ 1", cat1 - (cat1 % 1.0))
 
 print("способ 2", np.floor(cat1))
 """
-import numpy as np
 
-quote = "If Socialism can only be realized when the intellectual development of all the people permits it, then we shall not see Socialism for at least five hundred years"
-
-words = quote.split(" ")
-
-#print(words)
-
-selected = []
-
-for onewo in words:
-	#print(onewo)
-	lecount = 0
-	for letter in onewo:
-		#print(letter)
-		selected.append(letter)
-		lecount += 1
-		if lecount == 2:
-			break
-		else:
-			nothing =  "nothing"
-
-#print(selected)
-result =  "".join(selected)
-print(result, "\n")
-print(quote)
 
 
 
