8b6b17056b53eb9a9d8d99fcbf7814a0e5b58f02
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2023-03-26 21:20:51 +0300

    summation titles and abstracts

--- get_abstr_and_titles.py
+++ get_abstr_and_titles.py
@@ -1,3 +1,33 @@
+# ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓   get abstracts   ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+with open("павлов22.txt", "r") as text1:
+    row = "starting_value"
+    this_row_belongs_to_abstract = False
+
+    all_abstracts_words = []
+
+    while row != "":
+        row = text1.readline()
+        row_cat = row.split(" ")
+        #CONDITION1
+        if row_cat[0] ==   "Abstract:":
+            this_row_belongs_to_abstract = True
+        elif row_cat[0] == "Keywords:":
+            this_row_belongs_to_abstract = False
+        elif row ==        "\n":
+            this_row_belongs_to_abstract = False
+        else:
+            condition1 = "else1"
+
+        #CONDITION2
+        if this_row_belongs_to_abstract is True:
+            all_abstracts_words = all_abstracts_words + row_cat
+        else:
+            condition2 = "else2"
+
+print("\n\n всего слов во всех абстрактах:", len(all_abstracts_words) )
+#print(all_abstracts_words)
+# ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑   get abstracts   ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
 # ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓   get titles   ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 '''
 oblifrodoc2  = obtained_line_from_doc2
@@ -5,11 +35,7 @@ liwialtiwo   = list_with_all_titles_words
 oblifrodoc2  = obtained_line_from_doc2
 liwiwofrobli = list_with_words_from_obtained_line
 liwialtiwo   = list_with_all_titles_words
-
-
 '''
-
-
 with open("павлов22.txt", "r") as doc2:
     liwialtiwo = []
 
@@ -39,9 +65,22 @@ with open("павлов22.txt", "r") as doc2:
             condition3 = "else3"
 
 print("\n\n всего слов во названиях:", len(liwialtiwo) )
-print(liwialtiwo)
-
+#print(liwialtiwo)
 # ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑   get titles   ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
+# ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓   summation   ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+abstracts_AND_titles_words = all_abstracts_words + liwialtiwo
+print("\n\n всего слов в названиях И абстрактах:", len(abstracts_AND_titles_words) )
 
+
+
+from collections import Counter
+words_repeat1 = dict(Counter(abstracts_AND_titles_words))
+print(words_repeat1)
+
+import pandas as pd
+df1 = pd.DataFrame.from_dict(words_repeat1, orient = 'index')
+print(df1)
+#df1.to_csv("result1.csv")
+# ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑   summation   ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
