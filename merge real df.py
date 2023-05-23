3fd72a170a3108c521ba83c3c557f35432e158b4
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2023-05-23 11:29:53 +0300

    ready to concat

--- merge real df.py
+++ merge real df.py
@@ -1,26 +1,60 @@
 import pandas as pd
-
-#задать названия колонок
+#create df
 df_20 = pd.read_csv("2020.txt.csv")
-#df_21 = pd.read_csv("2021.txt.csv")
-#df_22 = pd.read_csv("2022.txt.csv")
+df_21 = pd.read_csv("2021.txt.csv")
+df_22 = pd.read_csv("2022.txt.csv")
+#задать названия колонок
 df_20.columns = ["СЛОВО", "ВСТРЕТИЛОСЬ_РАЗ_в_2020"]
-#df_21.columns = ["СЛОВО", "ВСТРЕТИЛОСЬ_РАЗ_в_2021"]
-#df_22.columns = ["СЛОВО", "ВСТРЕТИЛОСЬ_РАЗ_в_2022"]
-
-
-
-#перевернуть
+df_21.columns = ["СЛОВО", "ВСТРЕТИЛОСЬ_РАЗ_в_2021"]
+df_22.columns = ["СЛОВО", "ВСТРЕТИЛОСЬ_РАЗ_в_2022"]
+#перевернуть таблицы
+#
 df20_tr = df_20.transpose()
-print("*"*50, "перевернут")
-print(df20_tr.info())
-
-df20_tr.to_csv("transposed.csv")
-print(df20_tr.head())
-#----------------------------поосле этого вручную редактировать файл
-
-
-
+df20_tr.to_csv("2020_tr.csv")
+#
+df21_tr = df_21.transpose()
+df21_tr.to_csv("2021_tr.csv")
+#
+df22_tr = df_22.transpose()
+df22_tr.to_csv("2022_tr.csv")
+#отредактировать таблицы - вынести колонки
+#
+with open("2020_tr.csv", "r+") as csvtext1:
+    csv_str1 = csvtext1.read()
+    stop = csv_str1.index("СЛОВО")
+    for masu1 in range(stop):
+        symbol = csv_str1[0]
+        csv_str1 = csv_str1.replace(symbol, "", 1)
+    csvtext1.close()
+new_file = open("2020_tr_col.csv",'w')
+new_file.write(csv_str1)
+new_file.close()
+df_20_tr_col = pd.read_csv("2020_tr_col.csv")
+#
+with open("2021_tr.csv", "r+") as csvtext1:
+    csv_str1 = csvtext1.read()
+    stop = csv_str1.index("СЛОВО")
+    for masu1 in range(stop):
+        symbol = csv_str1[0]
+        csv_str1 = csv_str1.replace(symbol, "", 1)
+    csvtext1.close()
+new_file = open("2021_tr_col.csv",'w')
+new_file.write(csv_str1)
+new_file.close()
+df_21_tr_col = pd.read_csv("2021_tr_col.csv")
+#
+with open("2022_tr.csv", "r+") as csvtext1:
+    csv_str1 = csvtext1.read()
+    stop = csv_str1.index("СЛОВО")
+    for masu1 in range(stop):
+        symbol = csv_str1[0]
+        csv_str1 = csv_str1.replace(symbol, "", 1)
+    csvtext1.close()
+new_file = open("2022_tr_col.csv",'w')
+new_file.write(csv_str1)
+new_file.close()
+df_22_tr_col = pd.read_csv("2022_tr_col.csv")
+#объединение
 
 '''
 with open("transposed.csv", "r+") as csvtext1:
