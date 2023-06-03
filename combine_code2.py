609a26a24cb355949f81e1631aa511e962abc455
yourlittleaimeeiswagginghertail <skolnikivan1@gmail.com>
2023-06-03 20:37:17 +0300

    std TWICE

--- combine_code2.py
+++ combine_code2.py
@@ -294,7 +294,7 @@ plt.xticks(rotation=85)
 plt.legend()
 plt.show()
 
-#standart deviation
+#standart deviation FIRST
 df_stand = df_obs
 
 for col_name in [hamburger1, hamburger2]:
@@ -302,10 +302,19 @@ for col_name in [hamburger1, hamburger2]:
     mu = np.average(column)
     sigma = np.std(column)
     df_stand[col_name] = column[ (column>=(mu-sigma))&(column<=(mu+sigma)) ]
-
 df_stand_drn = df_stand.dropna()
-print("df after standart deviation \n\n", df_stand_drn, stars)
+print("df after standart deviation FIRST \n\n", df_stand_drn, stars)
 
+#standart deviation SECOND
+df_stand = df_stand_drn
+
+for col_name in [hamburger1, hamburger2]:
+    column = df_stand[col_name]
+    mu = np.average(column)
+    sigma = np.std(column)
+    df_stand[col_name] = column[ (column>=(mu-sigma))&(column<=(mu+sigma)) ]
+df_stand_drn = df_stand.dropna()
+print("df after standart deviation SECOND \n\n", df_stand_drn, stars)
 
 
 
